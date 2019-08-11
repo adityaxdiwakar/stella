ce8a553f1f9c3d98112cf32b2741b6718984d64a
Aditya Diwakar <aditya@diwakar.io>
2019-08-11 12:05:32 -0400

    Added space autocorrect forgiveness

--- main.py
+++ main.py
@@ -15,13 +15,21 @@ class Stella(discord.Client):
         print('Logged on as', self.user)
 
     async def on_message(self, message):
+        message.content = message.content.lower()
         if message.content.startswith("?"):
-                if message.content.lower().startswith("?ping"):
-                    await ping.main(message)
+            if message.content[1] == " ":
+                char_array = list(message.content)
+                del char_array[1]
+                message.content = ""
+                for char in char_array:
+                    message.content += char
 
-                elif message.content.lower().startswith("?c"):
-                    await charts.main(message)
-                    
+            if message.content.startswith("?ping"):
+                await ping.main(message)
+
+            elif message.content.startswith("?c"):
+                await charts.main(message)
+                
         
 ctx = Stella()
 ctx.run(os.getenv("BOT_TOKEN"))
