bdcd6e68d035530dcea7e40b81efe464fc90a17e
Aditya Diwakar <aditya@diwakar.io>
2019-08-11 12:28:13 -0400

    Removed stray code

--- commands/ping.py
+++ commands/ping.py
@@ -4,8 +4,6 @@ import time
 async def main(message, canary=False):
     premsgs = ["**[Canary]** ", " "]
     premsg = premsgs[not canary]
-    if canary == True:
-        premsg = "**[Canary]** "
     msg = await message.channel.send(premsg + f":ping_pong: Calculating ping...")
     roundtrip = round((msg.created_at.timestamp() - message.created_at.timestamp()) * 1000)
     await msg.edit(content=premsg + f":ping_pong: WS roundtrip complete in {roundtrip}ms!")
