9cbc88653c3c7168097f164ed8493c482394c190
Aditya Diwakar <aditya@diwakar.io>
2019-10-04 23:09:38 -0400

    Add test for ng_all command

---
+++ commands/ng_all.py
@@ -0,0 +1,10 @@
+from utils import all_chart_history as ach
+from datetime import datetime
+import time
+
+async def main(message, canary=False):
+    premsgs = ["**[Canary]** ", " "]
+    premsg = premsgs[not canary]
+    msg = await message.channel.send("Generating chart, stand by.")
+    chart_link = ach.main()
+    await msg.edit(content=premsg + f"Here you go: {chart_link}")
---
+++ utils.py/all_chart_history.py
@@ -0,0 +1,28 @@
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+import requests
+import os
+import time
+
+def main():
+    try:
+        positions = [(1, 1), (1, 2), (2, 1), (2, 2)]
+        reports = ["gfs", "gfs-ensemble", "ecmwf-ensemble", "ecmwf"]
+        fig = make_subplots(rows=2, cols=2, subplot_titles=reports, shared_yaxes=True, vertical_spacing=0.02)
+        for y in range(len(reports)):
+            r = requests.get(f"https://api.adi.wtf/weather/reports/{reports[y]}").json()
+            for x in r:
+                fig.add_trace(
+                    go.Scatter(x=list(r[x]["hdd"].keys()), y=list(r[x]["hdd"].values()), name=x, line_shape='spline'),
+                    row= positions[y][0], col = positions[y][1]
+                )    
+
+        fig.update_layout(template="plotly_dark", title="All Report Charts", width=3840, height=2160)
+
+        rn = time.time()
+        fig.write_image(f"{os.getenv('sys_file_location')}/{rn}.jpeg")
+
+        return f"{os.getenv('file_location')}/{rn}.jpeg"
+    except Exception as e:
+        return f"```\n{e}\n```"
