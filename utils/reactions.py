1d1b32dcaff88a203b83d73247bca9048d609bc4
Aditya Diwakar <aditya@diwakar.io>
2020-03-22 00:28:26 -0400

    Add multi-role feature

--- utils/reactions.py
+++ utils/reactions.py
@@ -1,15 +1,15 @@
 reaction_config = {
     632602390985703424: { # verification channel 
-        "💰": 632600011280089108 # moneybag and relevant role
+        "💰": [632600011280089108, 691138779171258368] # moneybag and relevant role
     },
     691130032051060766: {
-        "🕐": 691116986272579614,
-        "🛶": 691117034699751464,
-        "📆": 691117056313262111,
-        "🖱️": 691117080472453161,
-        "🌉": 691117127389675561,
-        "💵": 691117221556125789,
-        "📈": 691117223179190333
+        "🕐": [691116986272579614],
+        "🛶": [691117034699751464],
+        "📆": [691117056313262111],
+        "🖱️": [691117080472453161],
+        "🌉": [691117127389675561],
+        "💵": [691117221556125789],
+        "📈": [691117223179190333]
     }
 }
 
@@ -24,12 +24,12 @@ async def handler(ctx, payload, state):
 
     g_id = payload.guild_id
     u_id = payload.user_id
-    r_id = reaction_config[m_id][e_name]
     guild = ctx.get_guild(g_id)
-    role = guild.get_role(r_id)
     member = guild.get_member(u_id)
 
-    if state == "add":
-        await member.add_roles(role, reason="Reaction request fulfillment.")
-    else:
-        await member.remove_roles(role, reason="Reaction request fulfillment.")
+    for r_id in reaction_config[m_id][e_name]:
+        role = guild.get_role(r_id)
+        if state == "add":
+            await member.add_roles(role, reason="Reaction request fulfillment.")
+        else:
+            await member.remove_roles(role, reason="Reaction request fulfillment.")
