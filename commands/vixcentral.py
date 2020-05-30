326f66a549b56236f7ad677f466b75bc2bcf571f
Aditya Diwakar <aditya@diwakar.io>
2020-05-30 08:52:44 -0400

    Added fix for nil-valued months

--- commands/vixcentral.py
+++ commands/vixcentral.py
@@ -4,12 +4,13 @@ import requests
 import time
 import json
 
+
 async def main(message, canary=False):
     try:
         premsg = ["**[Canary]** ", " "][not canary]
 
         msg = await message.channel.send(premsg + "Okay, standby while the chart is generated.")
-        
+
         r = requests.get("http://vixcentral.com/ajax_update", headers={
             "X-Requested-With": "XMLHttpRequest",
         })
@@ -22,40 +23,46 @@ async def main(message, canary=False):
         contract_months = r_json[0]
         futures_prices = r_json[2]
         index_prices = r_json[8]
+
+        futures_prices = [x for x in futures_prices if x != 0]
+        contract_months = contract_months[:len(futures_prices)]
+        index_prices = contract_months[:len(futures_prices)]
+
         index_text = [None for x in range(len(index_prices))]
         index_text[-1] = index_prices[0]
 
         fig = go.Figure()
 
-        fig.add_trace(go.Scatter(x=contract_months, 
-                                y=futures_prices, 
-                                name="Futures",
-                                line_shape='spline', 
-                                mode='lines+markers+text',
-                                text=[str(x) for x in futures_prices],
-                                textposition="top center"))
-        
-        fig.add_trace(go.Scatter(x=contract_months, 
-                                y=index_prices, 
-                                name="VIX Index", 
-                                line=dict(
-                                    color="green",
-                                    dash='dash'
-                                ),
-                                line_shape='spline', 
-                                mode='lines+markers+text',
-                                text=index_text,
-                                textposition="bottom center"))
+        fig.add_trace(go.Scatter(x=contract_months,
+                                 y=futures_prices,
+                                 name="Futures",
+                                 line_shape='spline',
+                                 mode='lines+markers+text',
+                                 text=[str(x) for x in futures_prices],
+                                 textposition="top center"))
+
+        fig.add_trace(go.Scatter(x=contract_months,
+                                 y=index_prices,
+                                 name="VIX Index",
+                                 line=dict(
+                                     color="green",
+                                     dash='dash'
+                                 ),
+                                 line_shape='spline',
+                                 mode='lines+markers+text',
+                                 text=index_text,
+                                 textposition="bottom center"))
 
         fig.update_traces(textfont_size=18)
-        fig.update_layout(template="plotly_dark", title=f"VIX Calendar Curve for {r_json[1][0].split(' ')[0]}", width=1440)
+        fig.update_layout(template="plotly_dark",
+                          title=f"VIX Calendar Curve for {r_json[1][0].split(' ')[0]}", width=1440)
 
         rn = time.time()
         fig.write_image(f"/var/www/html/u/ca/{rn}.jpeg")
         link = f"https://img.adi.wtf/ca/{rn}.jpeg"
 
         await msg.edit(content=premsg + f"Here you are: {link}")
-        
+
     except Exception as e:
         print(e)
         await msg.edit(content=premsg + "Unfortunately, an error has occured. Developers have been notified of the issues.")
