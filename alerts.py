5518ebc7ab78a5ad8b2686d72571ccd3d73a8ab2
Aditya Diwakar <aditya@diwakar.io>
2020-06-30 12:48:47 -0400

    Added more alerts

--- alerts.py
+++ alerts.py
@@ -13,7 +13,14 @@ text = {
 	"open-futures": "🔔🔔🔔 The futures market is back open for the week! Have a good one!",
 	"close": "🔔🔔🔔 The cash market is now closed! Hope you had a great trading day!",
 	"close-futures": "🔔🔔🔔 The futures market is now closed for the weekend!",
-	"euro-open": "🔔🔔🔔 The LSE (London Stock Exchange) is now open"
+	"euro-open": "🔔🔔🔔 The LSE (London Stock Exchange) is now open",
+	"euro-close": "🔔🔔🔔 The LSE (London Stock Exchange) is now closed",
+	"tokyo-open": "🔔🔔🔔 The TSE (Tokyo Stock Exchange) is now open",
+	"tokyo-close": "🔔🔔🔔 The TSE (Tokyo Stock Exchange) is now closed",
+	"hk-open": "🔔🔔🔔 The HKE (Hong Kong Stock Exchange) is now open",
+	"hk-close": "🔔🔔🔔 The HKE (Hong Long Stock Exchange) is now closed",
+	"sse-open": "🔔🔔🔔 The SSE (Shanghai Stock Exchange) is now open",
+	"sse-close": "🔔🔔🔔 The SSE (Shanghai Stock Exchange) is now close"
 }[argument]
 
 for webhook in [os.getenv("WEBHOOK_1"), os.getenv("WEBHOOK_2")]:
