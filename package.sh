5f7aaa7ea55f6155139749b3b6b424e4a4178140
Aditya Diwakar <aditya@diwakar.io>
2020-07-25 02:07:41 -0400

    :wrench: More tests

--- package.sh
+++ package.sh
@@ -1,3 +1,2 @@
 echo "$DOCKER_PASSWORD" | docker login docker.pkg.github.com -u "$DOCKER_USERNAME" --password-stdin 
-docker tag stella docker.pkg.github.com/adityaxdiwakar/stella:$TRAVIS_TAG
-docker push docker.pkg.github.com/adityaxdiwakar/stella:$TRAVIS_TAG
+docker push adityaxdiwakar/stella
