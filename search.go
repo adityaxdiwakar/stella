fbb39f793e64cdf0687c2baa45e1e2f5ec58ac49
Aditya Diwakar <aditya@diwakar.io>
2020-07-26 12:55:12 -0400

    :sparkles: Remove OTC from search

--- search.go
+++ search.go
@@ -26,12 +26,24 @@ func searchTicker(s *discordgo.Session, m *discordgo.MessageCreate, mSplit []str
 		return
 	}
 
+	validResponses := 0
 	response := fmt.Sprintf("Relevant responses to your query `%s`:", query)
 	for _, instrument := range searchResponse.Value.Instruments {
 		if strings.HasPrefix(instrument.Symbol, "0") {
 			continue
 		}
+
+		if instrument.SourceType != "NYSE" && instrument.SourceType != "NASDAQ" {
+			continue
+		}
+
 		response += fmt.Sprintf("\n- (**`%s`**) %s", instrument.Symbol, instrument.Description)
+		validResponses++
+	}
+
+	if validResponses == 0 {
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("No tickers available for your query: `%s`", query))
+		return
 	}
 
 	s.ChannelMessageSend(m.ChannelID, response)
