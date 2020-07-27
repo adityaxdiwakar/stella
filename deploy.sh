ed146072f4a5b28b7dc7f5b4100dc23c80e38c28
Aditya Diwakar <aditya@diwakar.io>
2020-07-26 22:13:42 -0400

    :robot: Use deploy_rsa rather than id_rsa

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@ docker login docker.pkg.github.com -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 make push-image
 
 eval "$(ssh-agent -s)"
-chmod 600 .travis/id_rsa
-ssh-add .travis/id_rsa
+chmod 600 .travis/deploy_rsa
+ssh-add .travis/deploy_rsa
 
 ssh $USER@$IP << EOF
     cd $DEPLOY_DIR
