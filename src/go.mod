de164c2a503800e0650dd1c39e079efd3f2367c7
Aditya Diwakar <aditya@diwakar.io>
2021-06-03 22:45:20 -0400

    :tada: Begin work on image generation

--- src/go.mod
+++ src/go.mod
@@ -8,8 +8,10 @@ require (
 	github.com/adityaxdiwakar/tda-go v0.0.0-20200720003608-6e71f979b4cb // indirect
 	github.com/bwmarrin/discordgo v0.21.1
 	github.com/go-redis/redis/v8 v8.0.0-beta.6
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/google/go-querystring v1.0.0
 	github.com/jinzhu/copier v0.0.0-20190924061706-b57f9002281a
 	github.com/lib/pq v1.7.0
+	golang.org/x/image v0.0.0-20210504121937-7319ad40d33e // indirect
 	golang.org/x/text v0.3.3
 )
--- src/quotes.go
+++ src/quotes.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	"io/ioutil"
+	"os"
 	"time"
 
 	"github.com/adityaxdiwakar/flux"
 	"github.com/bwmarrin/discordgo"
+	"github.com/golang/freetype"
+	"github.com/golang/freetype/truetype"
+	"golang.org/x/image/math/fixed"
 )
 
 func quoteTicker(s *discordgo.Session, m *discordgo.MessageCreate, mSplit []string, count int) {
@@ -36,10 +45,6 @@ func quoteTicker(s *discordgo.Session, m *discordgo.MessageCreate, mSplit []stri
 		},
 	})
 
-	fmt.Println(mSplit[1], err)
-	if err == nil {
-		fmt.Println(len(searchResponse.Items))
-	}
 	if err != nil || len(searchResponse.Items) == 0 {
 		s.ChannelMessageSend(m.ChannelID, "Something went wrong while processing the request!")
 		return
@@ -57,27 +62,113 @@ func quoteTicker(s *discordgo.Session, m *discordgo.MessageCreate, mSplit []stri
 		}
 	}
 
-	var sChange string
-	var sPercent string
-	if payload.NETCHANGE > 0 {
-		sChange = fmt.Sprintf("+%.2f", payload.MARKCHANGE)
-		sPercent = fmt.Sprintf("+%.2f%%", payload.MARKPERCENTCHANGE*100)
-	} else {
-		sChange = fmt.Sprintf("%.2f", payload.MARKCHANGE)
-		sPercent = fmt.Sprintf("%.2f%%", payload.MARKPERCENTCHANGE*100)
+	photoFile, err := os.Open("Quote 1x.png")
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
 
-	responseText := printer.Sprintf(("__Quote Information for %s__\n" +
-		"%.2f %s (%s)\n\n" +
-		"**Last:** %.2f (x%d)\n" +
-		"**Bid:** %.2f (x%d)\n" +
-		"**Ask:** %.2f (x%d)\n" +
-		"**Volume:** %d"),
-		searchResponse.Items[0].Symbol,
-		payload.MARK, sChange, sPercent,
-		payload.LAST, payload.LASTSIZE,
-		payload.BID, payload.BIDSIZE,
-		payload.ASK, payload.ASKSIZE,
-		payload.VOLUME)
-	s.ChannelMessageSend(m.ChannelID, string(responseText))
+	i, _, err := image.Decode(photoFile)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	img := i.(*image.NRGBA)
+
+	ttf, err := ioutil.ReadFile("OpenSans-Regular.ttf")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	font, err := truetype.Parse(ttf)
+
+	start := time.Now()
+	c := freetype.NewContext()
+	size := 24.0
+
+	greenColor := color.RGBA{66, 169, 43, 255}
+	redColor := color.RGBA{255, 51, 51, 255}
+
+	var primaryColor image.Image
+	if payload.MARKCHANGE > 0 {
+		primaryColor = image.NewUniform(greenColor)
+	} else if payload.MARKCHANGE < 0 {
+		primaryColor = image.NewUniform(redColor)
+	} else {
+		primaryColor = image.White
+	}
+
+	c.SetDPI(72)
+	c.SetFont(font)
+	c.SetClip(img.Bounds())
+	c.SetSrc(image.White)
+	c.SetFontSize(size)
+	c.SetDst(img)
+
+	// add ticker
+	tickerStr := searchResponse.Items[0].Symbol
+	addLabel(tickerStr, c, font, 65, 75, image.White, 24.0, 140)
+
+	// add mark
+	markStr := printer.Sprintf("%.2f", payload.MARK)
+	addLabel(markStr, c, font, 65, 220, primaryColor, 24.0, 500)
+
+	// mark change
+	markChStr := printer.Sprintf("%.2f (%.2f%%)", payload.MARKCHANGE, payload.MARKPERCENTCHANGE*100)
+	addLabel(markChStr, c, font, 65, 394, primaryColor, 24.0, 500)
+
+	// bid string
+	bidStr := printer.Sprintf("%.2f", payload.BID)
+	addLabel(bidStr, c, font, 65, 568, primaryColor, 24.0, 500)
+
+	// ask string
+	askStr := printer.Sprintf("%.2f", payload.ASK)
+	addLabel(askStr, c, font, 65, 698, primaryColor, 24.0, 500)
+
+	// volume
+	volumeStr := printer.Sprintf("%d", payload.VOLUME)
+	addLabel(volumeStr, c, font, 65, 875, image.White, 24.0, 500)
+
+	buff := new(bytes.Buffer)
+	png.Encode(buff, img)
+
+	file := discordgo.File{
+		Name:   "file.png",
+		Reader: bytes.NewReader(buff.Bytes()),
+	}
+
+	s.ChannelMessageSendComplex(m.ChannelID, &discordgo.MessageSend{
+		Files: []*discordgo.File{&file},
+	})
+
+	fmt.Println(time.Now().Sub(start))
+}
+
+func addLabel(str string, c *freetype.Context, font *truetype.Font, height int, offset int, color image.Image, size float64, space int) {
+	width, nSize := calculateWidth(str, c, font, size, space)
+	pt := freetype.Pt(offset-(width/2).Round(), height+int(c.PointToFixed(size)>>6))
+	c.SetFontSize(nSize)
+	c.SetSrc(color)
+	c.DrawString(str, pt)
+
+}
+
+func calculateWidth(str string, c *freetype.Context, font *truetype.Font, size float64, space int) (fixed.Int26_6, float64) {
+	face := truetype.NewFace(font, &truetype.Options{
+		Size: size,
+	})
+
+	twidth := c.PointToFixed(0)
+	for _, ch := range str {
+		awidth, _ := face.GlyphAdvance(rune(ch))
+		twidth += awidth
+	}
+
+	if twidth.Floor() > (space - 15) {
+		return calculateWidth(str, c, font, float64(space-15)/float64(twidth.Floor())*float64(size), space)
+	}
+
+	return twidth, size
 }
